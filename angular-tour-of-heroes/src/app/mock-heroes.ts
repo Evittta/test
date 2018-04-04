831da2470c521490e44674f2b2a11eda38771229
Evittta <vita.kusinya@gmail.com>
2018-04-04 15:47:48 +0300

    rename heroes

--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -2,10 +2,10 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
     { id: 11, name: 'Mr.Nice' },
-    { id: 12, name: 'Evita' },
-    { id: 13, name: 'Oleg' },
-    { id: 14, name: 'Olya' },
-    { id: 15, name: 'Ivan' },
+    { id: 12, name: 'Andriy' },
+    { id: 13, name: 'Anton' },
+    { id: 14, name: 'Danil' },
+    { id: 15, name: 'Evita' },
     { id: 16, name: 'Katia' },
     { id: 17, name: 'Stepan' },
     { id: 18, name: 'Kim' },
