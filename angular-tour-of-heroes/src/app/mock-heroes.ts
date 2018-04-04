e8bb6c63e58420c5dc6a0fbc9eae1b538bdcd234
Evittta <vita.kusinya@gmail.com>
2018-04-04 13:19:47 +0300

    add files

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr.Nice' },
+    { id: 12, name: 'Evita' },
+    { id: 13, name: 'Oleg' },
+    { id: 14, name: 'Olya' },
+    { id: 15, name: 'Ivan' },
+    { id: 16, name: 'Katia' },
+    { id: 17, name: 'Stepan' },
+    { id: 18, name: 'Kim' },
+    { id: 19, name: 'Gepart' },
+    { id: 20, name: 'Jo' }
+];
